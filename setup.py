37fb44e569324bd3c9c08d642d2b8bdc96c79a66
Abdullah Mustapha <a41622100@gmail.com>
2024-02-01 13:52:22 +0000

    Updated the setup release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Notepad',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/abdullahcoder-tech/Notepad/',
+    license='https://github.com/abdullahCoder-Tech/Notepad/blob/main/LICENSE ',
+    author='Abdullah Mustapha',
+    author_email='abdullah.mustpaha@aol.com',
+    description='Text editor'
+)
